31fed078aceb0cf6bde68c2aab7a36e230fe7b40
mpelszyk <marcin.pelszyk90@gmail.com>
2021-06-14 21:00:50 +0200

    s

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+from .utils import get_or_set_order_session
 
 from .models import Product
 
